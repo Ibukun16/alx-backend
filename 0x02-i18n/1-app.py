f2e91e74e5a0ec6c6591775a11922028936216d0
Ibukun16 <ib.agunbiade18@gmail.com>
2024-11-05 23:00:46 +0000

    Update changes to task 0 & task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Instatiate Babel object"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Use render to return 1-index"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,1 @@
+This project directory contains advanced practice tasks on  the use of flask api. These tasks test understanding of the application of flask api. This skill is essential for an aspiring backend and full stack developer

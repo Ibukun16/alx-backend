a813c7c73c0f3dc85275626444d48ccb92f9a106
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-04 04:20:42 +0100

    update the changes and return correct result

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server:', error.toString());
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (error, reply) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertoSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');

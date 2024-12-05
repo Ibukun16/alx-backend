24e49ea59f9deb09baeae9a9a0a46148548012c6
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-05 03:57:12 +0100

    Update project repository

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,99 @@
+#!/usr/bin/yarn test
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { createQueue } from 'kue';
+import createPushNotificationsJobs from './8-jobs.js';
+
+describe('createPushNotificationsJobs', () => {
+  const spyMonitor = sinon.spy(console);
+  const QUEUE = createQueue({ name: 'push_notification_code_test' });
+
+  before(() => {
+    QUEUE.testMode.enter(true);
+  });
+
+  after(() => {
+    QUEUE.testMode.clear();
+    QUEUE.testMode.exit();
+  });
+
+  afterEach(() => {
+    spyMonitor.log.resetHistory();
+  });
+
+  it('displays an error message if jobs is not an array', () => {
+    expect(
+      createPushNotificationsJobs.bind(createPushNotificationsJobs, {}, QUEUE),
+    ).to.throw('Jobs is not an array');
+  });
+
+  it('add jobs to the queue with the correct type', (done) => {
+    expect(QUEUE.testMode.jobs.length).to.equal(0);
+    const jobDetails = [
+      {
+        phoneNumber: '+2348068617588',
+        message: 'Use the code 1617 to verify your account',
+      },
+      {
+        phoneNumber: '+2347068278800',
+        message: 'Use the code 1956 to verify your account',
+      },
+    ];
+    createPushNotificationsJobs(jobDetails, QUEUE);
+    expect(QUEUE.testMode.jobs.length).to.equal(2);
+    expect(QUEUE.testMode.jobs[0].data).to.deep.equal(jobDetails[0]);
+    expect(QUEUE.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    QUEUE.process('push_notification_code_3', () => {
+      expect(
+        spyMonitor.log.calledWith(
+          'Notification job created:',
+          QUEUE.testMode.jobs[0].id,
+        ),
+      ).to.be.true;
+      done();
+    });
+  });
+
+  it('registers the progress event handler for a job', (done) => {
+    QUEUE.testMode.jobs[0].addListener('progress', () => {
+      expect(
+        spyMonitor.log.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          '25% complete',
+        ),
+      ).to.be.true;
+      done();
+    });
+    QUEUE.testMode.jobs[0].emit('progress', 25);
+  });
+
+  it('registers the failed event handler for a job', (done) => {
+    QUEUE.testMode.jobs[0].addListener('failed', () => {
+      expect(
+        spyMonitor.log.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          'failed:',
+          'Failed to send',
+        ),
+      ).to.be.true;
+      done();
+    });
+    QUEUE.testMode.jobs[0].emit('failed', new Error('Failed to send'));
+  });
+
+  it('registers the complete event handler for a job', (done) => {
+    QUEUE.testMode.jobs[0].addListener('complete', () => {
+      expect(
+        spyMonitor.log.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          'completed',
+        ),
+      ).to.be.true;
+      done();
+    });
+    QUEUE.testMode.jobs[0].emit('complete');
+  });
+});
